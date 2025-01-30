2457dc404112a64e2faad2d9d7e30dfea98589eb
Yue Liu <mxj4@outlook.com>
2025-01-29 23:19:53 -0800

    fix: notify helper

--- btrbk-shed/usr/share/btrbk-shed/Notify.pm
+++ btrbk-shed/usr/share/btrbk-shed/Notify.pm
@@ -4,7 +4,8 @@ use v5.40;
 use strict;
 use warnings;
 
-sub compile_dbus_msg ($notify_id, $summary, $body, $timeout = 0) {
+sub compile_dbus_msg ($notify_id, $summary, $body, $critical = false, $timeout = 0) {
+    #say $body;
     state $app_name = "btrbk-shed";
     # we don't want a icon in notification body,
     # this is different from desktop entry icon shown in notification header
@@ -16,34 +17,32 @@ sub compile_dbus_msg ($notify_id, $summary, $body, $timeout = 0) {
         'desktop-entry': <'$app_name'>
     });
     # gdbus command is in glib2 package
-    my $command = qq(
-        gdbus call --session \
-        --dest=org.freedesktop.Notifications \
-        --object-path=/org/freedesktop/Notifications \
-        --method=org.freedesktop.Notifications.Notify \
-        "$app_name" $notify_id "$msg_icon_name" \
-        "$summary" "$body" "$actions" "$hints" $timeout
-    );
-    $command =~ s/\s+/ /g; # Replace multiple whitespace with single space
-    $command =~ s/^\s|\s$//g;  # Remove leading/trailing whitespace
+    my $command = qq(gdbus call --session --dest=org.freedesktop.Notifications --object-path=/org/freedesktop/Notifications --method=org.freedesktop.Notifications.Notify "$app_name" $notify_id "$msg_icon_name" "$summary" "$body" "$actions" "$hints" $timeout);
     #say $command;
     return $command
 }
 
 # start a new notification and return notify id
 sub start ($user, $summary, $body) {
-    my $result = qx(systemd-run --user --machine=$user@.host --pipe ${\compile_dbus_msg(0, $summary, $body)});
+    my $result = qx(systemd-run --user --machine=$user@.host --pipe ${
+        \compile_dbus_msg(0, $summary, $body)
+    });
     #say "$result";
     my ($notify_id) = $result =~ /\(uint32 (\d+),\)/;
     return $notify_id
 }
 
 # update the notification
-sub update ($user, $notify_id, $summary, $body) {
-    qx(systemd-run --user --machine=$user@.host ${\compile_dbus_msg($notify_id, $summary, $body)})
+sub update ($user, $notify_id, $summary, $body, $critical = false) {
+    qx(systemd-run --user --machine=$user@.host ${
+        \compile_dbus_msg($notify_id, $summary, $body, $critical)
+    });
+    sleep(1); # make notification visible for at least 1 sec
 }
 
 # end the notification in 10s
-sub end ($user, $notify_id, $summary, $body) {
-    qx(systemd-run --user --machine=$user@.host ${\compile_dbus_msg($notify_id, $summary, $body, 10000)})
+sub end ($user, $notify_id, $summary, $body, $critical = false) {
+    qx(systemd-run --user --machine=$user@.host ${
+        \compile_dbus_msg($notify_id, $summary, $body, $critical, 10000)
+    })
 }
--- test_notify.pl
+++ test_notify.pl
@@ -4,18 +4,23 @@ use v5.40;
 use strict;
 use warnings;
 
-use FindBin;
-use lib "$FindBin::Bin/btrbk-shed/usr/share/btrbk-shed";
+# Support Fedora's minimal Perl installation
+BEGIN {
+    my $script_path = $0;
+    my ($script_dir) = $script_path =~ m{^(.*)/}; # extract the directory part
+    $script_dir = '.' unless defined $script_dir;
+    unshift @INC, "$script_dir/btrbk-shed/usr/share/btrbk-shed"; # our modules
+}
+
 use Notify;
 
 sub test {
-  my $notify_id = Notify::start($ENV{USER}, "start", "Test,\nline2.");
+  my $notify_id = Notify::start($ENV{NOTIFY_USER}, "start", "Test,\nline2.");
   say "Started a notification, notify_id is $notify_id.";
   for (reverse 1..5) {
-    Notify::update ($ENV{USER}, $notify_id, "update", "$_");
-    sleep(1);
+    Notify::update ($ENV{NOTIFY_USER}, $notify_id, "update", "$_");
   }
-  Notify::end($ENV{USER}, $notify_id, "end", "Done.");
+  Notify::end($ENV{NOTIFY_USER}, $notify_id, "end", "Done.\nline2\nline3\n");
   say "Ended notification $notify_id.";
 }
 
